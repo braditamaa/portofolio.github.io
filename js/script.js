025d82cb606b9e69b98b78dacc09b5a6195fa7ea
Bagus Rizky Aditama <Brizkyaditama@gmail.com>
2023-08-30 13:57:00 +0700

    initial commit

--- js/script.js
+++ js/script.js
@@ -18,33 +18,20 @@
 
     // Dark Mode
     let darkmode = document.querySelector('#darkmode');
+    let logo = document.getElementById('logo');
+
     darkmode.onclick = () => {
         if(darkmode.classList.contains('bx-moon')){
             darkmode.classList.replace('bx-moon','bx-sun');
+            logo.src = 'logo3.png';
             document.body.classList.add('active');
         }else{
             darkmode.classList.replace('bx-sun','bx-moon');
+            logo.src = 'logo1.png';
             document.body.classList.remove('active');
         }
     }
 
-    function sendEmail(){
-    
-        Email.send({
-            Host : "smtp.elasticemail.com",
-            Username : "adityagoel1011@gmail.com",
-            Password : "BED936848AC76AE71B3B93337EFD9E03BD03",
-            From : document.getElementById("email").value,
-            To : "adityagoel1011@gmail.com",
-            Subject : "Contact Form Enquiry",
-            Body : "Name : " + document.getElementById("name").value +
-                    "<br>Email : " +  document.getElementById("email").value +
-                    "<br>Phone Number : " +  document.getElementById("phone").value +
-                    "<br>Message : " +  document.getElementById("message").value 
-        }).then(
-          message => alert(message)
-        );
-    }
 
 
     
