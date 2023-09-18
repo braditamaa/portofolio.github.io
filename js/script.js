18f620af0052791ad4b89d2d6d0e368ee6ceabb9
Bagus Rizky Aditama <Brizkyaditama@gmail.com>
2023-09-18 14:21:07 +0700

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,37 +1,48 @@
-	// Sticky Navbar
-    let header = document.querySelector('header');
-    let menu = document.querySelector('#menu-icon');
-    let navbar = document.querySelector('.navbar');
+// Sticky Navbar
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
 
-    window.addEventListener('scroll', () => {
-        header.classList.toggle('shadow', window.scrollY > 0);
-    });
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
 
-    menu.onclick = () => {
-        navbar.classList.toggle('active');
+// Dark Mode
+let darkmode = document.querySelector("#darkmode");
+let logo = document.getElementById("logo");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    logo.src = "logo3.png";
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    logo.src = "logo1.png";
+    document.body.classList.remove("active");
+  }
+};
+
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let revealtop = reveals[i].getBoundingClientRect().top;
+    let revealpoint = 150;
+
+    if (revealtop < windowHeight - revealpoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
-    window.onscroll = () => {
-        navbar.classList.remove('active');
-    }
-
-
-    // Dark Mode
-    let darkmode = document.querySelector('#darkmode');
-    let logo = document.getElementById('logo');
-
-    darkmode.onclick = () => {
-        if(darkmode.classList.contains('bx-moon')){
-            darkmode.classList.replace('bx-moon','bx-sun');
-            logo.src = 'logo3.png';
-            document.body.classList.add('active');
-        }else{
-            darkmode.classList.replace('bx-sun','bx-moon');
-            logo.src = 'logo1.png';
-            document.body.classList.remove('active');
-        }
-    }
-
-
-
-    
+  }
+}
